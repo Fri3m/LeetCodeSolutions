0ca8e88397efd308ded63fb2b168f441bf936058
Ömer Can Şimşek <108125233+Fri3m@users.noreply.github.com>
2024-02-28 15:20:20 +0300

    Question513

---
+++ src/Problem513/Solution513.java
@@ -0,0 +1,40 @@
+package Problem513;
+
+public class Solution513 {
+    int max =-1;
+    int value=0;
+    // 0ms beats 100% of users with Java && 43.18MB beats 83.03% of users with Java
+    public int findBottomLeftValue(TreeNode root) {
+        lrn(root,0);
+        return max;
+    }
+    private void lrn(TreeNode node,int lastDepth){
+        if(lastDepth > max){
+            max = lastDepth;
+            value = node.val;
+        }
+        if(node.left != null){
+            lrn(node.left,lastDepth+1);
+        }
+        if(node.right != null){
+            lrn(node.right,lastDepth+1);
+        }
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {
+    }
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

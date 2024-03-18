0111cd64431eb0e54468642067f1d5a9dd2d99a3
Ömer Can Şimşek <108125233+Fri3m@users.noreply.github.com>
2024-03-18 14:27:31 +0300

    Problem450

---
+++ leetCodeProblems/Problem452MinimumNumberOfArrowsToBurstBallons/Solution452.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points = sorted(points, key=lambda x: x[1])
+        # print(points)
+        if len(points) == 0:
+            return 0
+        arrows = 1
+        lastEnd = points[0][1]
+        for x in points:
+            if x[0] > lastEnd:
+                arrows += 1
+                lastEnd = x[1]
+        return arrows
+
+
+
+print(Solution().findMinArrowShots([[10, 16], [2, 8], [1, 6], [7, 12]]))
+print(Solution().findMinArrowShots([[1, 2], [3, 4], [5, 6], [7, 8]]))
+print(Solution().findMinArrowShots([[1, 2], [2, 3], [3, 4], [4, 5]]))
